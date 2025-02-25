3e4b1df25c8c9ed6694a97b5be863b76a9b080f8
codetrance <18899954+codetrance@users.noreply.github.com>
2025-02-25 08:54:21 +0530

    02_exercise complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (name1, repeats) {
+  let name = "";
+  if (repeats >= 0) {
+    for (i = 0; i < repeats; i++) {
+      name += name1;
+    }
+    return name;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
